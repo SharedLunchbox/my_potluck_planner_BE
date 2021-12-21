ec3b08cf4f8dddf1f41a0bca214c04424250f9b2
IrvinA <arevalosirvin@gmail.com>
2021-12-21 14:19:45 -0800

    Basic api up, no endpoints yet

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const server = require('./api/server.js');
+
+const { PORT } = require('./config');
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
